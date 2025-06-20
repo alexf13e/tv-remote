6a50bef27d394af5562cdc65ec4d4a7ce21a6b77
alexf13e <alex.w.faulkner13@gmail.com>
2025-06-20 17:17:46 +0100

    removed youview remote

--- main/actionlist.h
+++ main/actionlist.h
@@ -158,35 +158,6 @@ void createActionLists()
         WAIT_FOR_MS(5000),
         REPEAT_SIGNAL_FOR_MS(RemoteSpeakers::BLUETOOTH, 5000)
     };
-
-    ALL_ACTION_LISTS["STARTUP_YOUVIEW"] = {
-        RemoteTV::INPUT_AV,
-        RemoteTV::NAV_DOWN,
-        RemoteTV::NAV_DOWN,
-        RemoteTV::OK,
-        RemoteYouView::POWER,
-        RemoteSpeakers::YOUVIEW,
-        WAIT_FOR_MS(5000)
-    };
-
-    ALL_ACTION_LISTS["SHUTDOWN_YOUVIEW"] = {
-        RemoteYouView::POWER,
-        RemoteSpeakers::TV,
-        RemoteTV::INPUT_AV,
-        RemoteTV::NAV_UP,
-        RemoteTV::NAV_UP,
-        RemoteTV::OK
-    };
-
-    ALL_ACTION_LISTS["YOUVIEW_RECORDINGS"] = {
-        RemoteYouView::CLOSE,
-        WAIT_FOR_MS(1000),
-        RemoteYouView::HOME,
-        WAIT_FOR_MS(1000),
-        RemoteYouView::NAV_DOWN,
-        RemoteYouView::NAV_DOWN,
-        RemoteYouView::OK
-    };
     
 
 
@@ -221,19 +192,6 @@ void createActionLists()
         RemoteHDR::KEY_9,
     };
 
-    ActionRemoteSignal* YOUVIEW_NUMS[] = {
-        RemoteYouView::KEY_0,
-        RemoteYouView::KEY_1,
-        RemoteYouView::KEY_2,
-        RemoteYouView::KEY_3,
-        RemoteYouView::KEY_4,
-        RemoteYouView::KEY_5,
-        RemoteYouView::KEY_6,
-        RemoteYouView::KEY_7,
-        RemoteYouView::KEY_8,
-        RemoteYouView::KEY_9,
-    };
-
     for (const auto& channel : TV_CHANNELS)
     {
         //want to create action list of channel numbers with keys on remote.
@@ -241,7 +199,6 @@ void createActionLists()
 
         std::string tv_action_list_id = "TV_" + channel.first;
         std::string hdr_action_list_id = "HDR_" + channel.first;
-        std::string youview_action_list_id = "YOUVIEW_" + channel.first;
         
         std::string digits = channel.second;
         if (digits.size() < 3)
@@ -253,7 +210,6 @@ void createActionLists()
         {
             ALL_ACTION_LISTS[tv_action_list_id].push_back(TV_NUMS[c - '0']);
             ALL_ACTION_LISTS[hdr_action_list_id].push_back(HDR_NUMS[c - '0']);
-            ALL_ACTION_LISTS[youview_action_list_id].push_back(YOUVIEW_NUMS[c - '0']);
         }
     }
 
@@ -470,8 +426,8 @@ void createActionLists()
         RemoteSpeakers::SLEEP
     };
 
-    ALL_ACTION_LISTS["SPEAKERS_YOUVIEW"] = { //YouView
-        RemoteSpeakers::YOUVIEW
+    ALL_ACTION_LISTS["SPEAKERS_CBL_SAT"] = {
+        RemoteSpeakers::CBL_SAT
     };
 
     ALL_ACTION_LISTS["SPEAKERS_HDR"] = { //HDR
@@ -839,189 +795,6 @@ void createActionLists()
 
     ALL_ACTION_LISTS["HDR_MANUAL_SKIP_PLUS_60"] = {
         RemoteHDR::MANUAL_SKIP_PLUS_60
-    };
-
-
-    //////////////////////////////////////// YOUVIEW REMOTE ////////////////////////////////////////
-
-    ALL_ACTION_LISTS["YOUVIEW_POWER"] = {
-        RemoteYouView::POWER
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_ZOOM"] = {
-        RemoteYouView::ZOOM
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_TEXT"] = {
-        RemoteYouView::TEXT
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_HELP"] = {
-        RemoteYouView::HELP
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_BT_PLAYER"] = {
-        RemoteYouView::BT_PLAYER
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_HOME"] = {
-        RemoteYouView::HOME
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_RED"] = {
-        RemoteYouView::RED
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_GREEN"] = {
-        RemoteYouView::GREEN
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_YELLOW"] = {
-        RemoteYouView::YELLOW
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_BLUE"] = {
-        RemoteYouView::BLUE
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_INFO"] = {
-        RemoteYouView::INFO
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_VOL_UP"] = {
-        RemoteYouView::VOL_UP
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_VOL_DOWN"] = {
-        RemoteYouView::VOL_DOWN
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_VOL_MUTE"] = {
-        RemoteYouView::VOL_MUTE
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_CHAN_UP"] = {
-        RemoteYouView::CHAN_UP
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_CHAN_DOWN"] = {
-        RemoteYouView::CHAN_DOWN
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_NAV_UP"] = {
-        RemoteYouView::NAV_UP
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_NAV_DOWN"] = {
-        RemoteYouView::NAV_DOWN
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_NAV_LEFT"] = {
-        RemoteYouView::NAV_LEFT
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_NAV_RIGHT"] = {
-        RemoteYouView::NAV_RIGHT
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_OK"] = {
-        RemoteYouView::OK
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_BACK"] = {
-        RemoteYouView::BACK
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_REWIND"] = {
-        RemoteYouView::REWIND
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_PLAY"] = {
-        RemoteYouView::PLAY
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_PAUSE"] = {
-        RemoteYouView::PAUSE
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_FAST_FORWARD"] = {
-        RemoteYouView::FAST_FORWARD
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_SKIP_BACK"] = {
-        RemoteYouView::SKIP_BACK
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_RECORD"] = {
-        RemoteYouView::RECORD
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_STOP"] = {
-        RemoteYouView::STOP
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_SKIP_FORWARD"] = {
-        RemoteYouView::SKIP_FORWARD
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_GUIDE"] = {
-        RemoteYouView::GUIDE
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_SEARCH"] = {
-        RemoteYouView::SEARCH
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_CLOSE"] = {
-        RemoteYouView::CLOSE
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_KEY_0"] = {
-        RemoteYouView::KEY_0
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_KEY_1"] = {
-        RemoteYouView::KEY_1
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_KEY_2"] = {
-        RemoteYouView::KEY_2
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_KEY_3"] = {
-        RemoteYouView::KEY_3
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_KEY_4"] = {
-        RemoteYouView::KEY_4
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_KEY_5"] = {
-        RemoteYouView::KEY_5
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_KEY_6"] = {
-        RemoteYouView::KEY_6
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_KEY_7"] = {
-        RemoteYouView::KEY_7
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_KEY_8"] = {
-        RemoteYouView::KEY_8
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_KEY_9"] = {
-        RemoteYouView::KEY_9
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_AD"] = {
-        RemoteYouView::AD
-    };
-    
-    ALL_ACTION_LISTS["YOUVIEW_DELETE_SUB"] = {
-        RemoteYouView::DELETE_SUB
     };
 }
 
